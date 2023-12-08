6c4c17ccdcb1f860847c563893a1fbf382aef49b
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2023-12-08 17:38:19 +0200

    Day 8 solutions

--- day8/index.py
+++ day8/index.py
@@ -1,43 +1,89 @@
+import math
 
-import re
 
-numbers = {
-  "one": "1",
-  "two": "2",
-  "three": "3",
-  "four": "4",
-  "five": "5",
-  "six": "6",
-  "seven": "7",
-  "eight": "8",
-  "nine": "9",
-}
+def lcm(*args):
+  res = args[0]
+  for x in args:
+    res = (res * x) // math.gcd(res, x)
+  return res
 
-def handle_match(m):
-  for group in m.groups():
-    if group != None:
-      return numbers[group]
+class Grid:
+  def __init__(self, file):
+    instructions, content = open(file).read().split("\n\n")
 
-def replace_and_calc(file, repl_words = False):
-  content = open(file).read()
-  print(len(content.split("\n")))
+    instructions = [0 if c == "L" else 1 for c in instructions]
 
-  if repl_words:
-    num_re = re.compile(f"(?={'|'.join(map(lambda x: f'({x})', numbers.keys()))})")
-    content = num_re.sub(handle_match, content)
-  content = "".join(re.findall("\d|\n",content))
+    nodes = {}
+    for line in content.replace("(", "").replace(")", "").split("\n"):
+      name, neighbors = line.split(" = ")
+      nodes[name] = tuple(neighbors.split(", "))
 
-  sum = 0
-  print(len(content.split("\n")))
-  for line in content.split("\n"):
-    sum += int(line[0]+line[len(line)-1])
+    self.instructions = instructions
+    self.nodes = nodes   
 
-  return sum
+  def get_distance(self, start, target):
+    distance = 0
+    curr = start
 
-# Step 1
-# print("Test", replace_and_calc("./day1/test.txt"))
-# print("Data", replace_and_calc("./day1/data.txt"))
+    visited = {}
 
-# # Step 2
-print("Test", replace_and_calc("./day1/test2.txt", True))
-print("Data", replace_and_calc("./day1/data.txt", True))
+    while curr != target or distance == 0:
+      next_i = distance % len(self.instructions)
+      
+      key = f"{curr}-{next_i}"
+
+      if key in visited:
+        return 0
+
+      visited[key] = True
+
+      curr = self.nodes[curr][self.instructions[next_i]]
+      distance += 1
+    
+    return distance
+  
+  def has_path(self, start, target, visited):
+    if start == target:
+      return True
+    
+    for next in [self.nodes[start][0], self.nodes[start][1]]:
+      edge = f"{start}-{next}"
+      if edge in visited:
+        continue
+      visited[edge] = True
+
+      if self.has_path(next, target, visited):
+        return True
+
+    return False
+
+  
+  def compute_distances(self):
+    starts = list(filter(lambda k: k[2] == "A", self.nodes.keys()))
+    ends = list(filter(lambda k: k[2] == "Z", self.nodes.keys()))
+
+    distances = []
+    for start in starts:
+      for end in ends:
+        if self.has_path(start, end, {}):
+          distances.append(self.get_distance(start, end))
+          break
+    
+    return lcm(*distances)
+
+def run1(file):
+  grid = Grid(file)
+  d = grid.get_distance("AAA", "ZZZ")
+  return d
+
+def run2(file):
+  grid = Grid(file)
+  return grid.compute_distances()
+
+
+
+print(run1("./day8/test.txt"))
+print(run1("./day8/data.txt"))
+
+print(run2("./day8/test2.txt"))
+print(run2("./day8/data.txt"))
