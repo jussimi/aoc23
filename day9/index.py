9863281b06ad58a9d5e9432a75051bc5dc8e790e
Jussi Mäki-Ikola <jussimakiikola@gmail.com>
2023-12-09 17:33:02 +0200

    Day 9 solutions

--- day9/index.py
+++ day9/index.py
@@ -1,43 +1,26 @@
 
-import re
+def is_zeroes(row):
+  for item in row:
+    if item != 0: return False
+  return True
 
-numbers = {
-  "one": "1",
-  "two": "2",
-  "three": "3",
-  "four": "4",
-  "five": "5",
-  "six": "6",
-  "seven": "7",
-  "eight": "8",
-  "nine": "9",
-}
+def extrapolate(line):
+  history = [[int(x) for x in line.split()]]
 
-def handle_match(m):
-  for group in m.groups():
-    if group != None:
-      return numbers[group]
+  latest = history[0]
+  while not is_zeroes(latest):
+    latest = [latest[i] - latest[i-1] for i in range(1, len(latest))]
+    history.append(latest)
 
-def replace_and_calc(file, repl_words = False):
-  content = open(file).read()
-  print(len(content.split("\n")))
+  history[-1].extend([0, 0])
+  for i in range(len(history) - 2, -1, -1):
+    history[i].append(history[i][-1] + history[i+1][-1])
+    history[i].insert(0, history[i][0] - history[i+1][0])
 
-  if repl_words:
-    num_re = re.compile(f"(?={'|'.join(map(lambda x: f'({x})', numbers.keys()))})")
-    content = num_re.sub(handle_match, content)
-  content = "".join(re.findall("\d|\n",content))
+  return (history[0][0], history[0][-1])
 
-  sum = 0
-  print(len(content.split("\n")))
-  for line in content.split("\n"):
-    sum += int(line[0]+line[len(line)-1])
+def run(file):
+  return [sum(x) for x in zip(*[extrapolate(l) for l in open(file).read().split("\n")])]
 
-  return sum
-
-# Step 1
-# print("Test", replace_and_calc("./day1/test.txt"))
-# print("Data", replace_and_calc("./day1/data.txt"))
-
-# # Step 2
-print("Test", replace_and_calc("./day1/test2.txt", True))
-print("Data", replace_and_calc("./day1/data.txt", True))
+print(run("./day9/test.txt"))
+print(run("./day9/data.txt"))
